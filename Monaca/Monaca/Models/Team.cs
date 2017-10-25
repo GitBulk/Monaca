6008f129138fe782369aba9dc1772723c3707ff9
gitbulk <giacatiensinh@hotmail.com>
2017-10-25 23:58:44 +0700

    [f] add models

--- Monaca/Monaca/Models/Team.cs
+++ Monaca/Monaca/Models/Team.cs
@@ -1,7 +1,32 @@
+using System;
+using System.Collections.Generic;
+
 namespace Monaca.Models
 {
     public class Team
     {
-        
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Member> Members { get; set; }
+
+        public Team()
+        {
+            this.Members = new List<Member>();
+        }
+
+        public Team(string name):this()
+        {
+            this.Name = name;
+        }
+
+        public Team(string name, Guid id):this(name)
+        {
+            this.Id = id;
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
     }
 }
